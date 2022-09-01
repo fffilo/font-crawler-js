00aba0b281c4608207d2f4323dea839fc57d5e37
Franjo Filo <fffilo666@gmail.com>
2022-09-01 13:41:11 +0200

    Deprecate asyncMax option - automatically request animation frame

--- font-crawler.js
+++ font-crawler.js
@@ -89,11 +89,15 @@
             filter: null,
 
             /**
-             * Elements to process on single async interval.
+             * Duration of each async interval (in milliseconds).
+             *
+             * Math.floor(1000/60)
+             *
+             * ...one second has 1000ms, divided by 60fps.
              *
              * @type {Number}
              */
-            asyncMax: 1000,
+            asyncIntervalDuration: 16,
         },
 
         /**
@@ -114,8 +118,10 @@
          * @return {Void}
          */
         destroy: function() {
+            this.clear();
+
             delete this._options;
-            delete this._element;
+            delete this._target;
         },
 
         /**
@@ -137,6 +143,16 @@
         },
 
         /**
+         * Is async crawl pending.
+         *
+         * @todo
+         * @return {Boolean}
+         */
+        get isPending() {
+            return !!this.__frameInterval;
+        },
+
+        /**
          * Get option.
          *
          * @param  {String} key
@@ -152,11 +168,10 @@
          * @return {Object}
          */
         crawl: function() {
-            return this._elementWithPseudoEntries().reduce(function(carry, entry) {
-                var element = entry[0],
-                    pseudo = entry[1];
+            this._checkBusy();
 
-                return this._handleElement(element, pseudo, carry);
+            return this._getEntries().reduce(function(carry, entry) {
+                return this._handleEntry(entry, carry);
             }.bind(this), {});
         },
 
@@ -167,11 +182,24 @@
          * @return {Void}
          */
         crawlAsync: function(callback) {
-            var count = this.getOption("asyncMax")*1;
-            if (!count || count < 0)
-                count = this._defaults.asyncMax;
+            this._checkBusy();
 
-            this._crawlAsyncRecursive(this._elementWithPseudoEntries(), count, {}, callback);
+            this._getEntriesAsync(function(entries) {
+                this._crawlAsyncRequestFrame(entries, {}, callback);
+            });
+        },
+
+        /**
+         * Clear all (cancel async crawl).
+         *
+         * @return {Void}
+         */
+        clear: function() {
+            if (this.__frameInterval)
+                this._cancelAnimationFrame(this.__frameInterval);
+
+            delete this.__frameTimestamp;
+            delete this.__frameInterval;
         },
 
         /**
@@ -179,39 +207,214 @@
          *
          * @return {Array}
          */
-        _elementWithPseudoEntries: function(elements) {
-            var target = this.target,
-                selector = this.getOption("querySelector"),
-                includeTarget = this.getOption("includeTarget"),
-                pseudoElements = this.getOption("usePseudoElements"),
-                useTextNodesOnly = this.getOption("useTextNodesOnly"),
-                filter = this.getOption("filter"),
-                children = target.querySelectorAll(selector),
-                elements = Array.prototype.slice.call(children);
-            if (includeTarget)
-                elements.splice(0, 0, target);
-            if (pseudoElements)
-                pseudoElements = pseudoElements.replace(/[^\w\-,]/g, "").split(",");
+        _getEntries: function() {
+            var elements = this._querySelectorElements();
+            elements = this._filterElements(elements);
 
-            return elements
-                .reduce(function(carry, element) {
-                    carry.push([ element, null ]);
-                    (pseudoElements || []).forEach(function(pseudo) {
-                        carry.push([ element, "::" + pseudo ]);
-                    });
+            var entries = this._mapElementsAsEntries(elements);
+            entries = this._addPseudoEntries(entries);
+            entries = this._filterEntries(entries);
 
-                    return carry;
-                }, [])
-                .filter(function(entry) {
-                    if (useTextNodesOnly && !this._hasTextNodeChild(entry[0]))
-                        return false;
-
-                    return typeof filter === "function" ? filter.call(this, entry[0], entry[1]) : true;
-                }.bind(this));
+            return entries;
         },
 
         /**
-         * Get child nodes of element with node type text node.
+         * Get all element/pseudo entries async.
+         *
+         * @return {Array}
+         */
+        _getEntriesAsync: function(callback) {
+            this._querySelectorElementsAsync(function(elements) {
+                this._filterElementsAsync(elements, function(elements) {
+                    this._mapElementsAsEntriesAsync(elements, function(entries) {
+                        this._addPseudoEntriesAsync(entries, function(entries) {
+                            this._filterEntriesAsync(entries, function(entries) {
+                                callback.call(this, entries);
+                            });
+                        });
+                    });
+                });
+            });
+        },
+
+        /**
+         * Query selector elements:
+         * use querySelector option to find all elements on target, and
+         * include target if includeTarget option is set to true.
+         *
+         * @return {Array}
+         */
+        _querySelectorElements: function() {
+            var target = this.target,
+                selector = this.getOption("querySelector"),
+                includeTarget = this.getOption("includeTarget"),
+                nodeList = target.querySelectorAll(selector),
+                elements = Array.prototype.slice.call(nodeList);
+            if (includeTarget)
+                elements.splice(0, 0, target);
+
+            return elements;
+        },
+
+        /**
+         * Query selector elements asyc.
+         *
+         * @param  {Function} callback
+         * @return {Void}
+         */
+        _querySelectorElementsAsync: function(callback) {
+            this._delayExec(true, this._querySelectorElements, [], callback);
+        },
+
+        /**
+         * Filter elements:
+         * do not use elements without text nodes (if option useTextNodesOnly
+         * is set to true).
+         *
+         * @param  {Array} elements
+         * @return {Array}
+         */
+        _filterElements: function(elements) {
+            var useTextNodesOnly = this.getOption("useTextNodesOnly");
+            if (useTextNodesOnly)
+                elements = elements.filter(function(element) {
+                    return this._hasTextNodeChild(element);
+                }.bind(this));
+
+            return elements;
+        },
+
+        /**
+         * Filter elements async.
+         *
+         * @param  {Array}    elements
+         * @param  {Function} callback
+         * @return {Void}
+         */
+        _filterElementsAsync: function(elements, callback) {
+            this._delayExec(this.getOption("useTextNodesOnly"), this._filterElements, [ elements ], callback);
+        },
+
+        /**
+         * Map elements as entries:
+         * map each element as array of element and null (where null is pseudo
+         * element).
+         *
+         * @param  {Array} elements
+         * @return {Array}
+         */
+        _mapElementsAsEntries: function(elements) {
+            return elements.map(function(element) {
+                return [ element, null ];
+            });
+        },
+
+        /**
+         * Map elements as entries async.
+         *
+         * @param  {Array}    elements
+         * @param  {Function} callback
+         * @return {Void}
+         */
+        _mapElementsAsEntriesAsync: function(elements, callback) {
+            this._delayExec(false, this._mapElementsAsEntries, [ elements ], callback);
+        },
+
+        /**
+         * Add pseudo entries:
+         * expand list of entries with pseudo elements from usePseudoElements
+         * option.
+         *
+         * @param  {Array} entries
+         * @return {Array}
+         */
+        _addPseudoEntries: function(entries) {
+            var pseudoElements = this.getOption("usePseudoElements");
+            if (pseudoElements) {
+                pseudoElements = pseudoElements.replace(/[^\w\-,]/g, "").split(",");
+
+                return entries.reduce(function(carry, entry) {
+                    carry.push(entry);
+
+                    pseudoElements.forEach(function(pseudo) {
+                        carry.push([ entry[0], "::" + pseudo ]);
+                    });
+                }, []);
+            }
+
+            return entries;
+        },
+
+        /**
+         * Add pseudo entries async.
+         *
+         * @param  {Array}    entries
+         * @param  {Function} callback
+         * @return {Void}
+         */
+        _addPseudoEntriesAsync: function(entries, callback) {
+            this._delayExec(this.getOption("usePseudoElements"), this._addPseudoEntries, [ entries ], callback);
+        },
+
+        /**
+         * Filter entries:
+         * ...with method defined in filter option.
+         *
+         * @param  {Array} entries
+         * @return {Array}
+         */
+        _filterEntries: function(entries) {
+            var filter = this.getOption("filter");
+            if (typeof filter === "function")
+                entries = entries.filter(function(entry) {
+                    return filter.call(this, entry[0], entry[1]);
+                });
+
+            return entries;
+        },
+
+        /**
+         * Filter entries async.
+         *
+         * @param  {Array}    entries
+         * @param  {Function} callback
+         * @return {Void}
+         */
+        _filterEntriesAsync: function(entries, callback) {
+            this._delayExec(typeof this.getOption("filter") === "function", this._filterEntries, [ entries ], callback);
+        },
+
+        /**
+         * Delay method execution (request animation frame) if condition
+         * argument is truthy.
+         *
+         * @param  {Boolean}   condition
+         * @param  {Function}  method
+         * @param  {Array}     args
+         * @param  {Function}  callback
+         * @return {Void}
+         */
+        _delayExec: function(condition, method, args, callback) {
+            if (condition)
+                this._requestAnimationFrame(function() {
+                    callback.call(this, method.apply(this, args));
+                }.bind(this));
+            else
+                callback.call(this, method.apply(this, args));
+        },
+
+        /**
+         * Check if async crawler is pending and throw exception if so.
+         *
+         * @return {Void}
+         */
+        _checkBusy: function() {
+            if (this.isPending)
+                throw "FontCrawler: FontCrawler is busy.";
+        },
+
+        /**
+         * Element has text node child.
          *
          * @param  {HTMLElement} element
          * @return {Boolean}
@@ -247,30 +450,104 @@
         },
 
         /**
-         * Crawl async (with recursion).
+         * Cancel animation frame request.
+         *
+         * @param  {Number} requestID
+         * @return {Void}
+         */
+        _cancelAnimationFrame: function(requestID) {
+            this.window.cancelAnimationFrame(requestID);
+        },
+
+        /**
+         * Get current timestamp (in milliseconds).
+         *
+         * @return {Number}
+         */
+        _getTimestamp: function() {
+            return (new Date()).getTime() / 1000;
+        },
+
+        /**
+         * Request frame animation for async crawl.
          *
          * @param  {Array}    entries
-         * @param  {Number}   count
          * @param  {Object}   source
          * @param  {Function} callback
          * @return {Void}
          */
-        _crawlAsyncRecursive: function(entries, count, source, callback) {
-            this._requestAnimationFrame(function() {
-                var done = 0;
-                while (done++ < count && entries.length) {
-                    var entry = entries.shift(),
-                        element = entry[0],
-                        pseudo = entry[1];
+        _crawlAsyncRequestFrame: function(entries, source, callback) {
+            this.__frameInterval = this._requestAnimationFrame(this._crawlAsyncCallback.bind(this, entries, source, callback));
+        },
 
-                    this._handleElement(element, pseudo, source);
-                }
+        /**
+         * Async crawl callback: handle entry recursively.
+         *
+         * @param  {Array}    entries
+         * @param  {Object}   source
+         * @param  {Function} callback
+         * @return {Void}
+         */
+        _crawlAsyncCallback: function(entries, source, callback) {
+            if (!this.__frameTimestamp)
+                this.__frameTimestamp = this._getTimestamp();
 
-                if (entries.length)
-                    this._crawlAsyncRecursive.call(this, entries, count, source, callback);
-                else if (typeof callback === "function")
-                    callback.call(this, source);
-            }.bind(this));
+            this._handleEntry(entries.shift(), source);
+            this._crawlAsyncRecursion(entries, source, callback);
+        },
+
+        /**
+         * Async crawl recursion: request async crawl callback (see above) in
+         * current or next animated interval (or clear all on empty entries).
+         *
+         * @param  {Array}    entries
+         * @param  {Object}   source
+         * @param  {Function} callback
+         * @return {Void}
+         */
+        _crawlAsyncRecursion: function(entries, source, callback) {
+            if (!entries.length)
+                return this._crawlAsyncClear(source, callback);
+
+            var start = this.__frameTimestamp,
+                now = this._getTimestamp(),
+                delay = now - start,
+                asyncIntervalDuration = this.getOption("asyncIntervalDuration");
+            if (delay < asyncIntervalDuration/1000)
+                return this._crawlAsyncCallback(entries, source, callback);
+
+            this.clear();
+
+            return this._crawlAsyncRequestFrame(entries, source, callback);
+        },
+
+        /**
+         * Clear async crawl and execute callback.
+         *
+         * @param  {Object}   source
+         * @param  {Function} callback
+         * @return {Void}
+         */
+        _crawlAsyncClear: function(source, callback) {
+            this.clear();
+
+            if (typeof callback === "function")
+                this._requestAnimationFrame(callback.bind(this, source));
+        },
+
+        /**
+         * Entry handler:
+         * get element/pseudo and execute event handler.
+         *
+         * @param  {Array}  entry
+         * @param  {Object} source
+         * @return {Object}
+         */
+        _handleEntry: function(entry, source) {
+            var element = entry[0],
+                pseudo = entry[1];
+
+            return this._handleElement(element, pseudo, source);
         },
 
         /**
